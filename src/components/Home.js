88dba764829e3d228a57e10efea5f187ec81723d
adamprinciotta <adamprinciotta@gmail.com>
2023-03-15 15:38:15 -0400

    Set up header with animations

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,52 @@ import '../styles/Home.css';
 function Home() {
   return (
     <div>
-      <h1>Welcome to my website!</h1>
-      <p>This is the home page.</p>
+      {/* <h1>Welcome to my website!</h1> */}
+      <div className="intro">
+        <span>H</span>
+        <span>I</span>
+        <span>,</span>
+        <br />
+        <span>I</span>
+        <span>'</span>
+        <span>M</span>
+        <br />
+        <span className="A">A</span>
+        <span>D</span>
+        <span>A</span>
+        <span>M</span>
+        <span>&nbsp; </span>
+        <span>P</span>
+        <span>R</span>
+        <span>I</span>
+        <span>N</span>
+        <span>C</span>
+        <span>I</span>
+        <span>O</span>
+        <span>T</span>
+        <span>T</span>
+        <span>A</span>
+        <br/>
+        <span className="devTitle">F</span>
+        <span className="devTitle">R</span>
+        <span className="devTitle">O</span>
+        <span className="devTitle">N</span>
+        <span className="devTitle">T</span>
+        <span>&nbsp; </span>
+        <span className="devTitle">E</span>
+        <span className="devTitle">N</span>
+        <span className="devTitle">D</span>
+        <span>&nbsp; </span>
+        <span className="devTitle">D</span>
+        <span className="devTitle">E</span>
+        <span className="devTitle">V</span>
+        <span className="devTitle">E</span>
+        <span className="devTitle">L</span>
+        <span className="devTitle">O</span>
+        <span className="devTitle">P</span>
+        <span className="devTitle">E</span>
+        <span className="devTitle">R</span>
+      </div>
     </div>
   );
 }
