c280b616876b2e9fc153d63fbd85afb63b7b774b
adamprinciotta <adamprinciotta@gmail.com>
2023-03-16 11:27:50 -0400

    Added scroll down animation and about me section

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,35 +3,35 @@ import '../styles/Home.css';
 
 function Home() {
   return (
-    <div>
+    <><div>
       {/* <h1>Welcome to my website!</h1> */}
       <div className="intro">
-        <span>H</span>
-        <span>I</span>
-        <span>,</span>
+        <span className="bounce">H</span>
+        <span className="bounce">I</span>
+        <span className="bounce">,</span>
         <br />
-        <span>I</span>
-        <span>'</span>
-        <span>M</span>
+        <span className="bounce">I</span>
+        <span className="bounce">'</span>
+        <span className="bounce">M</span>
         <br />
         <div className="name">
-          <span className="A">A</span>
-          <span>D</span>
-          <span>A</span>
-          <span>M</span>
+          <span className="A bounce">A</span>
+          <span className="bounce">D</span>
+          <span className="bounce">A</span>
+          <span className="bounce">M</span>
           <span>&nbsp; </span>
-          <span>P</span>
-          <span>R</span>
-          <span>I</span>
-          <span>N</span>
-          <span>C</span>
-          <span>I</span>
-          <span>O</span>
-          <span>T</span>
-          <span>T</span>
-          <span>A</span>
+          <span className="bounce">P</span>
+          <span className="bounce">R</span>
+          <span className="bounce">I</span>
+          <span className="bounce">N</span>
+          <span className="bounce">C</span>
+          <span className="bounce">I</span>
+          <span className="bounce">O</span>
+          <span className="bounce">T</span>
+          <span className="bounce">T</span>
+          <span className="bounce">A</span>
         </div>
-        <br/>
+        <br />
         <span className="devTitle">F</span>
         <span className="devTitle">R</span>
         <span className="devTitle">O</span>
@@ -53,6 +53,33 @@ function Home() {
         <span className="devTitle">R</span>
       </div>
     </div>
+    
+    <div className="scrollDown"><span className="movingScroll">Scroll Down</span> --{">"}</div>
+    <div className="spacer"></div>
+
+    <div className="aboutMe">
+      <h1>About Me</h1>
+      <p>I'm a front-end developer with a passion for creating responsive and dynamic websites that deliver an exceptional user experience. 
+        <br/>
+        <br/>
+        With a strong eye for detail, I strive to design clean, elegant, and intuitive user interfaces that are both functional and visually appealing.
+        <br/>
+        <br/>
+        My expertise lies in front-end technologies such as HTML, CSS, and JavaScript, with a focus on the React library. 
+        <br/>
+        <br/>
+        I began my coding journey in high school and pursued my passion by studying computer science in college, where I discovered my love for front-end development.
+        <br/>
+        <br/>
+        I'm always eager to learn and grow, and I'm constantly seeking new opportunities to expand my skillset. The seemingly endless possibilities of programming amaze me, and I'm dedicated to staying up-to-date with the latest trends and technologies in the field.
+        <br/>
+        <br/>
+        {/* Make this link to the contact page */}
+        <a className="toContact">Let's work together to bring your web project to life!</a>
+        </p>
+    </div>
+    <div className="spacer"></div>
+    </>
   );
 }
 
